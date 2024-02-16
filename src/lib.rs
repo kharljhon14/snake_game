93f22ad9f2e4fbe7cf997c406944a72b36009908
kharle14 <enriquezkharl14@gmail.com>
2024-02-16 16:23:53 +0800

    Snake struct

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,34 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 pub struct World {
     width: usize,
+    snake: Snake,
+}
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec![SnakeCell(spawn_index)],
+        }
+    }
 }
 
 #[wasm_bindgen]
 impl World {
     pub fn new() -> World {
-        World { width: 8 }
+        World {
+            width: 8,
+            snake: Snake::new(10),
+        }
+    }
+
+    pub fn snake_head(&self) -> usize {
+        self.snake.body[0].0
     }
 
     pub fn set_width(&mut self, new_width: usize) {
