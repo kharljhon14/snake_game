84c695371154410085b7201819fcb849612e846a
kharle14 <enriquezkharl14@gmail.com>
2024-02-16 15:57:04 +0800

    HTML canvas and wrapper

--- www/index.js
+++ www/index.js
@@ -3,5 +3,8 @@ import init, { World } from 'snake_game';
 init().then(() => {
   const world = World.new();
 
-  console.log(world.get_width());
+  const canvas = document.getElementById('snake_canves');
+  const ctx = canvas.getContext('2d');
+
+  console.log(ctx);
 });
